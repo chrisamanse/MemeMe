3887bad7485d472f68ba16819a7ba8e4409901eb
Chris Amanse <chris@chrisamanse.xyz>
2016-10-16 03:52:15 -0700

    Create Meme.swift

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Chris Amanse on 10/16/2016.
+//  Copyright © 2016 Chris Amanse. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    
+    var image: UIImage
+    var memedImage: UIImage
+}
