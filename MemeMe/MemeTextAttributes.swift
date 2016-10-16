4e893368ee409c52555ce28194749bf9a9ba310e
Chris Amanse <chris@chrisamanse.xyz>
2016-10-15 18:22:46 -0700

    Create MemeTextAttributes.swift

---
+++ MemeMe/MemeTextAttributes.swift
@@ -0,0 +1,42 @@
+//
+//  MemeTextAttributes.swift
+//  MemeMe
+//
+//  Created by Chris Amanse on 10/15/2016.
+//  Copyright © 2016 Chris Amanse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public struct MemeTextAttributes {
+    public var font: UIFont = UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!
+    
+    public var strokeColor: UIColor = .black
+    public var strokeWidth: Float = -2.0
+    
+    public var foregroundColor: UIColor = .white
+    
+    public var alignment: NSTextAlignment = .center
+    
+    internal var paragraphStyle: NSParagraphStyle {
+        let style = NSMutableParagraphStyle()
+        style.alignment = alignment
+        
+        return style
+    }
+    
+    public init(font: UIFont) {
+        self.font = font
+    }
+    
+    public var textAttributes: [String: Any] {
+        return [
+            NSParagraphStyleAttributeName: paragraphStyle,
+            NSStrokeColorAttributeName: strokeColor,
+            NSStrokeWidthAttributeName: strokeWidth,
+            NSForegroundColorAttributeName: foregroundColor,
+            NSFontAttributeName: font
+        ]
+    }
+}
