df1940d742e4fb718652223536b41c48b0c25db7
Chris Amanse <chris@chrisamanse.xyz>
2016-10-15 17:24:21 -0700

    Create outlet for canvas UIView

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var canvasView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
