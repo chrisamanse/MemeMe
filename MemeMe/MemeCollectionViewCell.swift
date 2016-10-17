ba8ea5b6889c18ba858884358d5d2872b531c306
Chris Amanse <chris@chrisamanse.xyz>
2016-10-17 01:10:45 -0700

    Create MemeCollectionViewCell.swift

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Chris Amanse on 10/17/2016.
+//  Copyright © 2016 Chris Amanse. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+}
