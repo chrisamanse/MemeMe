4014b8a884b1ea73dd056d46fe401f00ed3afac9
Chris Amanse <chris@chrisamanse.xyz>
2016-10-16 23:17:16 -0700

    Create MemeTableViewCell and connect to storyboard

---
+++ MemeMe/MemeTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMe
+//
+//  Created by Chris Amanse on 10/16/2016.
+//  Copyright © 2016 Chris Amanse. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var memeLabel: UILabel!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+}
