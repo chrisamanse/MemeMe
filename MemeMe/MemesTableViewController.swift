4eef65f74877a047b60ed01c6c96d1429ae32aeb
Chris Amanse <chris@chrisamanse.xyz>
2016-10-16 23:00:59 -0700

    Create MemesTableViewController.swift

---
+++ MemeMe/MemesTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MemesTableViewController.swift
+//  MemeMe
+//
+//  Created by Chris Amanse on 10/16/2016.
+//  Copyright © 2016 Chris Amanse. All rights reserved.
+//
+
+import UIKit
+
+class MemesTableViewController: UITableViewController {
+    
+    let collection = MemeCollection.default
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return collection.memes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "memeCell", for: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+}
