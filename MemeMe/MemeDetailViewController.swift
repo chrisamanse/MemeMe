7b7d253fa1108f904743b86805b93d3b5b80d328
Chris Amanse <chris@chrisamanse.xyz>
2016-10-17 02:14:28 -0700

    Add MemeDetailViewController.swift

---
+++ MemeMe/MemeDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe
+//
+//  Created by Chris Amanse on 10/17/2016.
+//  Copyright © 2016 Chris Amanse. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+}
